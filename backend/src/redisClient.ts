2b60190cd2399b3a18a0a60445689bf2e51cccc5
Murali <muralisudireddy0@gmail.com>
2025-02-15 22:09:41 +0530

    re modified the redis error

--- backend/src/redisClient.ts
+++ backend/src/redisClient.ts
@@ -1,5 +1,7 @@
 import Redis from "ioredis";
+import dotenv from "dotenv"
 
+dotenv.config()
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.PORT ? parseInt(process.env.PORT) : undefined,  
