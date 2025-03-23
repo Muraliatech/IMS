681a8e5bdbc24126a53861e33348e186c502397d
Murali <muralisudireddy0@gmail.com>
2025-03-23 18:00:26 +0530

    Build

--- Frontend-Ims/vite.config.ts
+++ Frontend-Ims/vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react"
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
-
-
+  plugins: [react(),tailwindcss()], // No need to include Tailwind here
+  server: {
+    port: 5173, // You can change the port if needed
+  },
+  build: {
+    outDir: 'dist', // Default output folder
+  }
+});
