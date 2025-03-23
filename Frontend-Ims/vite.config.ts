fca3629db92f856692e30fd2cc8df274078eb937
Murali <muralisudireddy0@gmail.com>
2025-03-23 18:20:53 +0530

    vercel.json modified

--- Frontend-Ims/vite.config.ts
+++ Frontend-Ims/vite.config.ts
@@ -4,9 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()], // No need to include Tailwind here
   server: {
-    port: 5173, // You can change the port if needed
+    port: 5173,  
   },
-  build: {
-    outDir: 'dist', // Default output folder
-  }
+
 });
